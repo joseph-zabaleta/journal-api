00eea82c87a335132552c985314e6ed2e57ffc0e
joseph-zabaleta <joseph.l.zabaleta@gmail.com>
2020-07-07 15:57:13 -0700

    changed db to elephante

--- journal_project/config/settings.py
+++ journal_project/config/settings.py
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'jdeqlmqn',
+        'USER': 'jdeqlmqn',
+        'PASSWORD': 'LOxVl19uhasGaLTl33-wgq1eyXp5Qzbz',
+        'HOST': 'otto.db.elephantsql.com',
+        'PORT': 5432,
     }
 }
 
